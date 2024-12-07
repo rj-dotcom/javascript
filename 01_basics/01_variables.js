eb729d9d83f6a6fec4b927cc7efb396ce708441d
rj-dotcom <rajnsh70@gmail.com>
2024-12-07 11:19:55 +0000

    commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 15487
+let accountEmail = "raj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState;
+
+//accountId = 2
+accountEmail = "rj@rj.com"
+accountPassword = "25478"
+accountCity = "bihar"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
